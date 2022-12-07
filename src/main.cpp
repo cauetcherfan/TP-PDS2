fb99eb49b16f43958c465b337ab71cb0c229f7af
Matheus Salles <matt.salles@outlook.com>
2022-12-07 17:38:00 -0300

    implementando a main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
+#include <iostream>
 #include "bibliotecario.hpp"
 #include "cliente.hpp"
 #include "usuario.hpp"
 
 
 int main (){
+
+    Bibliotecario::Bibliotecario("Matheus", "123", "matheus@email.com", "1234-5678");
+
+    std::cout<<"Seja bem vindo(a) à Biblioteca Central"<<std::endl;
+    std::cout<<"Digite o seu nome"<<std::endl;
+    std::cin>>login;
+    std::cout<<"Digite a sua senha"<<std::endl;
+    std::cin>>senha;
+
+
     int rodando = 1;
-
-    
-
     while(rodando){
 
     }
