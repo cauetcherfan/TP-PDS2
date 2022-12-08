63e226443a4b5c8aaf99a1daae370172ece0c685
Maah-aquino <107276019+Maah-aquino@users.noreply.github.com>
2022-12-08 11:38:02 -0300

    Update usuario.cpp

--- src/usuario.cpp
+++ src/usuario.cpp
@@ -1,3 +1,56 @@
 #include <list>
 #include <string>
 #include "usuario.hpp"
+
+Usuario::Usuario(){};
+
+Usuario::Usuario(std::string nome, std::string email, std::string telefone) {
+	_nome = nome;
+	_email = email;
+	_telefone = telefone;
+}
+
+int Usuario::exibeMenu() const {
+	return -1;
+}
+
+
+bool Usuario::LogarComoAdmin() {
+	return _LogarComoAdmin;
+}
+
+std::string Usuario::getNome() {
+	
+	return _nome;
+}
+
+std::string Usuario::getSenha() {
+	return _senha;
+}
+
+std::string Usuario::getEmail() {
+	return _email;
+}
+
+std::string Usuario::getTelefone() {
+	return _telefone;
+
+void Usuario::setLogarComoAdmin(bool LogarComoAdmin) {
+	_LogarComoAdmin = LogarComoAdmin;
+}
+
+void Usuario::setNome(std::string nome) {
+	_nome = nome;
+}
+
+void Usuario::setSenha(std::string senha) {
+	_senha = senha;
+}
+
+void Usuario::setEmail(std::string email) {
+	_email = email;
+}
+
+void Usuario::setTelefone(std::string telefone) {
+	_telefone = telefone;
+}
